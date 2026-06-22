3fc8722e44ae45377f48bca087a260353a48e8b4
anasbughio <anasbughio@gmail.com>
2026-06-23 04:08:55 +0500

    resolve responsive issues

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,15 +15,15 @@ const Services = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-18.5">
         
         <div className="text-center mb-10">
-          <h2 className="font_family text-[rgb(0,0,0)] text-[45px] font-[700] leading-[49.5px] uppercase mb-0 font-serif">
+          <h2 className="font_family text-[rgb(0,0,0)] text-[32px] sm:text-[38px] md:text-[45px] font-[700] leading-[1.1] md:leading-[49.5px] uppercase mb-0 font-serif">
             Our Services
           </h2>
-          <p className="font_family text-[rgb(131,131,131)] text-[18px] leading-[24px]  max-w-2xl mx-auto">
+          <p className="font_family text-[rgb(131,131,131)] text-[15px] sm:text-[16px] md:text-[18px] leading-[22px] md:leading-[24px] max-w-2xl mx-auto mt-2 md:mt-0">
             This is Photoshop's version of Lorem Ipsum. Proin gravida
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-8 mb-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-8 mb-12 md:mb-24">
           
           <div className="flex items-start gap-2 cursor-pointer group">
             <div className="flex-shrink-0 w-14 h-22 bg-[#2eb8b8] -skew-x-[15deg] flex items-center justify-center shadow-sm transition-transform duration-300 group-hover:scale-110">
@@ -32,10 +32,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-center text-[20px] text-lg font-[400] tracking-none  uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
+              <h3 className="font_family text-center text-[18px] md:text-[20px] font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
                 Web Design
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -48,10 +48,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-[20px] text-center text-lg font-[400] tracking-none  uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
+              <h3 className="font_family text-[18px] md:text-[20px] text-center font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
                 Photography
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -64,10 +64,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-[20px] text-center text-lg font-[400] tracking-none  uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
+              <h3 className="font_family text-[18px] md:text-[20px] text-center font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
                 HTML5
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -80,10 +80,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-[20px] tracking-tighter text-center text-lg font-[400] tracking-none  uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
+              <h3 className="font_family text-[18px] md:text-[20px] tracking-tighter text-center font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
                 JQuery
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -96,10 +96,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-[20px] text-center text-lg font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
+              <h3 className="font_family text-[18px] md:text-[20px] text-center font-[400] tracking-none uppercase mb-1 transition-colors duration-300 group-hover:text-[#2eb8b8]">
                 SEO
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -112,10 +112,10 @@ const Services = () => {
               </div>
             </div>
             <div>
-              <h3 className="font_family text-[20px] text-[rgb(24, 24, 24)] text-center text-lg font-[400] tracking-none  uppercase mb-1  duration-300">
+              <h3 className="font_family text-[18px] md:text-[20px] text-[rgb(24,24,24)] text-center font-[400] tracking-none uppercase mb-1 duration-300">
                 CSS3
               </h3>
-              <p className="font_family text-center text-gray-400 text-[15px] leading-5">
+              <p className="font_family text-center text-gray-400 text-[14px] md:text-[15px] leading-5">
                 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris morbi accumsan.
               </p>
             </div>
@@ -124,12 +124,12 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="w-full flex justify-center relative mt-10">
+      <div className="w-full flex justify-center relative mt-6 md:mt-10">
         <div className="max-w-4xl w-full px-4 relative -bottom-1 drop-shadow-2xl opacity-90">
           <img 
             src={servicesMockup}
             alt="Multi-device mockup" 
-            className="w-full h-auto object-cover max-h-[300px] object-top rounded-t-xl"
+            className="w-full h-auto object-cover max-h-[190px] sm:max-h-[240px] md:max-h-[300px] object-top rounded-t-xl"
           />
         </div>
       </div>
